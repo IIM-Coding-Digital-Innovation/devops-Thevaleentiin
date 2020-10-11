f8c1db47ec6a12e7560bacfcb39f8440fd10d651
Thevaleentiin <mikado93220@gmail.com>
2020-10-11 18:11:05 +0200

    Revert "add Searchbar"
    This reverts commit 60fef4d66bfa8a0bdc81470d37bf28490769c0a7.

--- src/components/ApiMeteo.js
+++ src/components/ApiMeteo.js
@@ -1,13 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {getData} from './getData'
-import SearchBar from './SearchBar'
-import loader from '../img/loader.gif'
 
 function ApiMeteo () {
     const [data, setData] = useState([]);
     const [load, setLoad] = useState(false);
     const [error, setError] = useState(null);
-    const [valueInput, SetValue] = useState('');
     const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q=Paris&lang=fr&units=metric'
     const API_KEY  = '&appid=be724fe74c0763368fc2501fa7398520'
   
@@ -24,11 +21,9 @@ function ApiMeteo () {
     }, []);
 
 
-
     if (load) {
         return (
                 <>
-                    <SearchBar value={valueInput} />
                     {
                         error 
                         ? <li>{error.message}</li>
@@ -46,7 +41,7 @@ function ApiMeteo () {
     else {
         return (
             <>
-                <img src={loader} alt="loader" className="loader-page" /> 
+                <p>Chargement...</p>
             </>
         );
     }
